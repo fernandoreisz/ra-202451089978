2b5ae74206837a05fa3cb7a49ff9c692caca30e8
fernandoreisz <fernandocruzfjrcruz10@gmail.com>
2026-09-12 23:30:04 -0300

    Create Program.cs

---
+++ aaw/aulas/aula-06/LojaDev.NotificacaoApi/Program.cs
@@ -0,0 +1,1 @@
+var builder = WebApplication.CreateBuilder(args); builder.Services.AddControllers(); var app = builder.Build(); app.MapControllers(); Console.WriteLine("📧 NotificacaoApi rodando em http://localhost:5092"); app.Run();
